f573448e4b9aac1607213558372b00efb973fae9
Gordon Schulz <gordon.schulz@gmail.com>
2017-08-08 14:25:28 +0200

    Make HealthCheck a whole lot nicer

--- bin/healthcheck.sh
+++ bin/healthcheck.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/with-contenv /bin/bash
-curl -s --connect-timeout 2 http://${SYNCTHING_WEBUI_ADDRESS:-127.0.0.1}:{SYNCTHING_WEBUI_PORT}
+
+# if syncthing's webinterface is up we assume everything is fine
+curl -s -o /dev/null -w "%{http_code}" --connect-timeout 2 \
+    http://"${SYNCTHING_WEBUI_ADDRESS:-127.0.0.1}":"${SYNCTHING_WEBUI_PORT}"
