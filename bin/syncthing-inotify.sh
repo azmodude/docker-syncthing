8dfd3278604a706be40cd5190e0d073d8644c37e
Gordon Schulz <gordon.schulz@gmail.com>
2017-09-15 13:32:25 +0200

    Fix bash default variables
    The `-` was missing

--- bin/syncthing-inotify.sh
+++ bin/syncthing-inotify.sh
@@ -2,5 +2,5 @@
 SYNCTHING_API_KEY=$(cat /.apikey)
 
 exec s6-setuidgid "${RUN_USER}" syncthing-inotify \
-    -target="http://${SYNCTHING_WEBUI_ADDRESS:127.0.0.1}:${SYNCTHING_WEBUI_PORT}" \
+    -target="http://${SYNCTHING_WEBUI_ADDRESS:-127.0.0.1}:${SYNCTHING_WEBUI_PORT}" \
     -api="${SYNCTHING_API_KEY}"
--- bin/syncthing.sh
+++ bin/syncthing.sh
@@ -2,4 +2,5 @@
 SYNCTHING_API_KEY=$(cat /.apikey)
 
 exec s6-setuidgid "${RUN_USER}" syncthing -home="${SYNCTHING_HOME_DIRECTORY}" \
-    -gui-address="${SYNCTHING_WEBUI_ADDRESS:127.0.0.1}:${SYNCTHING_WEBUI_PORT}" -gui-apikey=${SYNCTHING_API_KEY} -no-browser
+    -gui-address="${SYNCTHING_WEBUI_ADDRESS:-127.0.0.1}:${SYNCTHING_WEBUI_PORT}" \
+    -gui-apikey="${SYNCTHING_API_KEY}" -no-browser
