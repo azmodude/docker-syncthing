eace601760250f507de736372b2ddbfcabb663cf
Gordon Schulz <azmo@zeus.azmo.ninja>
2018-06-25 14:11:43 +0200

    Fix stupid mistakes

--- rootfs/etc/cont-init.d/00-create-users-fix-permissions.sh
+++ rootfs/etc/cont-init.d/00-create-users-fix-permissions.sh
@@ -2,7 +2,7 @@
 
 >&2 echo "adding 'syncthing' with UID ${RUN_UID} and group 'syncthing' with GID ${RUN_GID}"
 groupadd -g "${RUN_GID}" syncthing || true && \
-    useradd -c 'Run User' -s /bin/bash -m -g "${RUN_GID}" -u "${RUN_UID}" syncthing || true
+    useradd -c 'Run User' -s /bin/bash -M -d /syncthing -g "${RUN_GID}" -u "${RUN_UID}" syncthing || true
 >&2 echo "fixing permissions"
-chown -R ${RUN_UID}:${RUN_GID} "${SYNCTHING_HOME_DIRECTORY}"
-chown ${RUN_UID}:${RUN_GID} "${SYNCTHING_DATA_DIRECTORY}"
+chown -R ${RUN_UID}:${RUN_GID} /syncthing
+chown ${RUN_UID}:${RUN_GID} /syncthing-data
